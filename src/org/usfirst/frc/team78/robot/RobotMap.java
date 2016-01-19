c611bbc13244f03b04b34f10f841f9836dd36044
seamus sullivan <meweight@cox.net>
2016-01-18 19:46:31 -0500

    monday

--- src/org/usfirst/frc/team78/robot/RobotMap.java
+++ src/org/usfirst/frc/team78/robot/RobotMap.java
@@ -14,6 +14,22 @@ public class RobotMap {
 	final static public int LEFT_DRIVE_2 = 1;
 	final static public int RIGHT_DRIVE_2 = 3;
 	
+	
+	//Analog Sensors
+	final static public int gyro = 0;
+	
+	//Constants
+	
+	final double heading_correction_const=(.032);
+	
+	//Digital IO
+	final static public int left_enc_a = 0;
+	final static public int left_enc_b = 0;
+	final static public int right_enc_a = 0;
+	final static public int right_enc_b = 0;
+	
+	
+	
     // For example to map the left and right motors, you could define the
     // following variables to use with your drivetrain subsystem.
     // public static int leftMotor = 1;
@@ -24,3 +40,4 @@ public class RobotMap {
     // public static int rangefinderPort = 1;
     // public static int rangefinderModule = 1;
 }
+w
